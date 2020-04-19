d7b782b42816dfd0636d8e6d9b861def2a1d9ca0
ibrahim-1303 <ibrahim.ayfer91@gmail.com>
2020-04-19 18:35:06 +0300

    Add project files.

---
+++ Business/Concrete/BookManager.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BookManager : IBookService
+    {
+        private IBookDal _bookDal;
+
+        public BookManager(IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+        public void Add(Book book)
+        {
+            if (_bookDal.Get(filter:b=> b.Name==book.Name &&  b.AuthorId == book.AuthorId)==null)
+            {
+                _bookDal.Add(book);
+            }
+            else
+            { 
+            throw new Exception(message: "Bu Kitap adı zaten mevcut");
+            }
+
+        }
+
+        public void Delete(Book book)
+        {
+            _bookDal.Delete(book);
+        }
+
+        public List<Book> Getall()
+        {
+            return _bookDal.GetList().ToList();
+        }
+
+        public List<Book> GetByAuthorId(int id)
+        {
+           return _bookDal.GetList(filter: b => b.AuthorId == id).ToList();
+        }
+
+        public Book GetById(int id)
+        {
+          return  _bookDal.Get(filter: b => b.Id == id);
+        }
+
+        public void Update(Book book)
+        {
+            _bookDal.Update(book);
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFrameWork/BookStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFrameWork
+{
+  public  class BookStoreContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=BookStore;Trusted_Connection=true");
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
